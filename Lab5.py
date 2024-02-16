674065d8a183aaeb59250241a417e5978d9cdcbe
bbq27 <133260826+bbq27@users.noreply.github.com>
2024-02-15 17:13:50 -0800

    Add files via upload

---
+++ Lab5.py
@@ -0,0 +1,48 @@
+# Name:
+# Section:
+#########################################################
+import data
+
+# Write your functions for each part in the space below.
+
+# Part 1
+   # The function for Part 1 should be within the class in data.py.
+
+
+# Part 2
+   # The function for Part 2 should be within the class in data.py.
+
+
+# Part 3
+''' Design Receipe
+input:
+output:
+purpose:
+Steps:
+'''
+
+
+# Part 4
+''' Design Receipe
+input:
+output:
+purpose:
+Steps:
+'''
+
+# Part 5
+''' Design Receipe
+input:
+output:
+purpose:
+Steps:
+'''
+
+# Part 6
+''' Design Receipe
+input:
+output:
+purpose:
+Steps:
+'''
+
---
+++ Lab5_tests.py
@@ -0,0 +1,35 @@
+# Name:
+# Section:
+#########################################################
+import data
+import Lab5
+import unittest
+
+
+# Write your test cases for each part below.
+
+class TestCases(unittest.TestCase):
+    # Part 1
+    #    Part 1 tests should be in data_tests.py.
+
+    # Part 2
+    #    Part 2 tests should be in data_tests.py.
+
+
+    # Part 3
+
+
+    # Part 4
+
+
+    # Part 5
+
+
+    # Part 6
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
